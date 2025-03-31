57453bc657e0bf7e3068cec82b8061e39083a818
/__Endrigo__ <113954684+Endrigogustavo@users.noreply.github.com>
2025-03-30 23:12:05 -0300

    Update App.java

--- FilaAtvTeste/src/App.java
+++ FilaAtvTeste/src/App.java
@@ -1,23 +1,39 @@
 public class App {
     public static void main(String[] args) {
         Fila filaTeste = new Fila();
+        System.out.println("Add dados");
+        System.out.println("-----------------------------------");
         filaTeste.enfileira("1");  
+        filaTeste.imprime();
+        System.out.println("-----------------------------------");
         filaTeste.enfileira("2"); 
+        filaTeste.imprime();
+        System.out.println("-----------------------------------");
         filaTeste.enfileira("3"); 
+        filaTeste.imprime();
+        System.out.println("-----------------------------------");
         filaTeste.enfileira("4"); 
         filaTeste.imprime();
-
         System.out.println("-----------------------------------");
 
-        filaTeste.desenfileira();
-        filaTeste.desenfileira();
-        filaTeste.desenfileira();
+       
+        System.out.println("\nRemove dados");
+        System.out.println("-----------------------------------");
+        System.out.println("Dados removidos: " + filaTeste.desenfileira() + "," + filaTeste.desenfileira() + "," + filaTeste.desenfileira());
+        
 
+        System.out.println("\nAdd new dados com a fila cheia:");
         filaTeste.enfileira("1"); 
+        filaTeste.imprime();
+        System.out.println("-----------------------------------");
         filaTeste.enfileira("2"); 
+        filaTeste.imprime();
+        System.out.println("-----------------------------------");
         filaTeste.enfileira("9"); 
         filaTeste.imprime();
+        System.out.println("-----------------------------------");
         
+        System.out.println("\nDados Vetor");
         System.out.println("-----------------------------------");
         System.out.println("Primeiro valor: " + filaTeste.primeiro());
         System.out.println("-----------------------------------");
