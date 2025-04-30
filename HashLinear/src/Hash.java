1bfbd1b6e63c4e0d7057b1ed96b640f8fe5f55e3
Endrigogustavo <endrigogustavo2006@gmail.com>
2025-04-30 12:11:59 -0300

    m

---
+++ HashLinear/src/Hash.java
@@ -0,0 +1,8 @@
+public class Hash {
+    public double item;
+    public boolean ocupado;
+    
+    public Hash(boolean b) { // construtor
+      ocupado = false;
+    }
+}
--- LivroPilhaEncadeado/src/Pilha.java
+++
@@ -1,6 +0,0 @@
-public class Pilha {
-    private Livro topo;
-
-    public void push(Livro livro){}
-    
-}
